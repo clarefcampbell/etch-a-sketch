e446b57e66aca45d180533202c9d59f9a4959829
Clare <110568186+clarefcampbell@users.noreply.github.com>
2023-03-22 20:17:34 +1300

    Add random color logic

--- script.js
+++ script.js
@@ -43,5 +43,10 @@ btn.addEventListener('click', () => {
 });
 
 function getColor() {
-    return 'black';
+
+    let r = Math.floor(Math.random()*256);
+    let g = Math.floor(Math.random()*256);
+    let b = Math.floor(Math.random()*256);
+    
+    return '#' + r.toString(16) + g.toString(16) + b.toString(16);
 }
