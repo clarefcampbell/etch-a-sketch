eec8c622ad4b171beca664fad3f0fdf47a52811a
Clare <110568186+clarefcampbell@users.noreply.github.com>
2023-03-22 19:54:11 +1300

    Make grid adjustable from user input

--- script.js
+++ script.js
@@ -1,23 +1,42 @@
 const container = document.getElementById('container');
-let gridSize = 16;
+let gridSize = 64;
 
-for (let i = 0; i < gridSize ; i++) {
-    let row = document.createElement('div');
-    row.className = 'row';
-    for (let j = 0; j < gridSize ; j++) {
-        let square = document.createElement('div');
-        square.className = 'square';
-        row.appendChild(square);
+function generateGrid (gridSize) {
+    for (let i = 0; i < gridSize ; i++) {
+        let row = document.createElement('div');
+        row.className = 'row';
+        for (let j = 0; j < gridSize ; j++) {
+            let square = document.createElement('div');
+            square.className = 'square';
+            row.appendChild(square);
+        }
+        container.appendChild(row);
     }
-    container.appendChild(row);
+    startHover()
+}
+document.addEventListener("DOMContentLoaded", generateGrid(gridSize));
+
+//Hover functionality
+function startHover() {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => square.addEventListener('mouseover', (e) => {
+        e.target.classList.add('mouseover');
+    }));
+
+    squares.forEach(square => square.addEventListener('mouseout', (e) => {
+        e.target.classList.remove('mouseover');
+    }));
 }
 
-const squares = document.querySelectorAll('.square');
+function clearGrid() {
+    const rows = document.querySelectorAll('.row');
+    rows.forEach(row => row.remove());
+}
 
-squares.forEach(square => square.addEventListener('mouseover', (e) => {
-    e.target.classList.add('mouseover');
-}));
-
-squares.forEach(square => square.addEventListener('mouseout', (e) => {
-    e.target.classList.remove('mouseover');
-}));
+//Changing grid size
+const btn = document.querySelector('button');
+btn.addEventListener('click', () => {
+    let userGridSize = prompt('Enter new grid size');
+    clearGrid();
+    generateGrid(userGridSize);
+});
