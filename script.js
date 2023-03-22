cc944f4d06e8f96a3845eca14216d64318900860
Clare <110568186+clarefcampbell@users.noreply.github.com>
2023-03-22 18:59:58 +1300

    Add mouseover effect

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ for (let i = 0; i < 16*16 ; i++) {
     square.className = 'square';
     container.appendChild(square);
 }
+
+const squares = document.querySelectorAll('.square');
+
+squares.forEach(square => square.addEventListener('mouseover', (e) => {
+    e.target.classList.add('mouseover');
+}));
+
+squares.forEach(square => square.addEventListener('mouseout', (e) => {
+    e.target.classList.remove('mouseover');
+}));
